68c3303dc176cab33b1b879b1cdf1da74946a65c
root <root@ntxvm020.iccluster.epfl.ch>
2017-03-30 09:13:16 +0200

    Fix module version collection
    * Apply http://stackoverflow.com/a/42763546/435004 to fix Array#to_h
      not existing under Ruby 2.0.x
    * Send errors of "puppet module list" to the bit bucket

--- lib/facter/puppet_module_versions.rb
+++ lib/facter/puppet_module_versions.rb
@@ -1,7 +1,15 @@
+unless [].respond_to? :to_h
+  class Array
+    def to_h
+      Hash[self]
+    end
+  end
+end
+
 # coding: utf-8
 Facter.add(:puppet_module_versions) do
   setcode do
-   module_list = Facter::Core::Execution.exec('puppet module list')
+   module_list = Facter::Core::Execution.exec('puppet module list 2>/dev/null')
    module_list.scan(/^.*? (\S+) \(\S*v([^\e]*)/m).to_h
   end
 end
