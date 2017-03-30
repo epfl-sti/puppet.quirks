9f52d7969a25fc5c0a693aedaae7d1f45cb42cea
root <root@ntxvm020.iccluster.epfl.ch>
2017-03-30 09:35:30 +0200

    Remove the 2>/dev/null after all
    68c3303dc176 fixes things the right way, so that there is no stderr
    stream anymore

--- lib/facter/puppet_module_versions.rb
+++ lib/facter/puppet_module_versions.rb
@@ -9,7 +9,7 @@ end
 # coding: utf-8
 Facter.add(:puppet_module_versions) do
   setcode do
-   module_list = Facter::Core::Execution.exec('puppet module list 2>/dev/null')
+   module_list = Facter::Core::Execution.exec('puppet module list')
    module_list.scan(/^.*? (\S+) \(\S*v([^\e]*)/m).to_h
   end
 end
