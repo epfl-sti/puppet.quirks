e70644c3a52204b60b790535c43f9cb146416ef7
Dominique Quatravaux <dominique@quatravaux.org>
2017-11-01 20:30:35 +0100

    Replayed changes for v0.4.1
    Specific revisions were lost in a VM snapshot mishap. The changelog was as follows:
    
    84ea094 master v0.4.1 Bump version for release
    4d1e433 Also sabotage debian.rb

--- manifests/private/subquirks/no_upstart_in_recent_ubuntu.pp
+++ manifests/private/subquirks/no_upstart_in_recent_ubuntu.pp
@@ -1,12 +1,16 @@
 # We don't want an "upstart" provider for the service type on recent Ubuntu.
 class quirks::private::subquirks::no_upstart_in_recent_ubuntu {
-  case "${::operatingsystem} ${::operatingsystemrelease}" {
-    /^Ubuntu 16/: {
-      file { [
-        "/usr/lib/ruby/vendor_ruby/puppet/provider/service/upstart.rb",
-        "/opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet/provider/service/upstart.rb"
-      ]:
-        ensure => "absent"
+  case $::operatingsystem {
+    "ubuntu": {
+      if (versioncmp($::operatingsystemmajrelease, "16") >= 0) {
+        file { [
+          "/usr/lib/ruby/vendor_ruby/puppet/provider/service/upstart.rb",
+          "/opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet/provider/service/upstart.rb",
+          "/usr/lib/ruby/vendor_ruby/puppet/provider/service/debian.rb",
+          "/opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet/provider/service/debian.rb"
+        ]:
+          ensure => "absent"
+        }
       }
     }
   }
